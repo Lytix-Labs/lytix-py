5e60d7894084a2c4474d948002ac9e1e62fa16bf
Sid Premkumar <sid.premkumar@gmail.com>
2024-08-12 21:06:41 -0400

    fix logging

--- src/lytix_py/LLogger/LLogger.py
+++ src/lytix_py/LLogger/LLogger.py
@@ -1,4 +1,5 @@
 import asyncio
+import json
 import logging
 import socket
 import sys
@@ -7,13 +8,20 @@ from typing import Mapping
 from lytix_py.LAsyncStore.LAsyncStore import LAsyncStore, LAsyncStoreClass
 from lytix_py.LLogger.LLoggerStreamWrapper import LLoggerStreamWrapper
 
+
 class LLogger:
     metadata: dict = None
 
     """
     @param config: Optional dict of the following shape: { console: boolean }
     """
-    def __init__(self, loggerName: str, metadata: dict = None, asyncStore: LAsyncStoreClass = None):
+
+    def __init__(
+        self,
+        loggerName: str,
+        metadata: dict = None,
+        asyncStore: LAsyncStoreClass = None,
+    ):
         self.logger = logging.getLogger(loggerName)
         if not len(self.logger.handlers):
             hostname = socket.gethostname()
@@ -21,10 +29,14 @@ class LLogger:
             """
             Setup formatting and logging config
             """
-            self.logger.setLevel('INFO')
-            fmt = '{"time": "%(asctime)s.%(msecs)03d",  "hostname": "' + hostname + '",  "level": "%(levelname)s", "msg": "%(message)s",  "name": "%(name)s", "pid": "%(process)d"}'
+            self.logger.setLevel("INFO")
+            fmt = (
+                '{"time": "%(asctime)s.%(msecs)03d",  "hostname": "'
+                + hostname
+                + '",  "level": "%(levelname)s", "msg": "%(message)s",  "name": "%(name)s", "pid": "%(process)d"}'
+            )
 
-            datefmt="%Y-%m-%d,%H:%M:%S"
+            datefmt = "%Y-%m-%d,%H:%M:%S"
             formatter = logging.Formatter(fmt, datefmt)
             handler = logging.StreamHandler(stream=sys.stdout)
             handler.setFormatter(formatter)
@@ -33,7 +45,6 @@ class LLogger:
             progress = LLoggerStreamWrapper(customAsyncStore=asyncStore)
             progress.setFormatter(formatter)
             self.logger.addHandler(progress)
-        
 
         """
         Define our async store to store recent logs
@@ -43,34 +54,36 @@ class LLogger:
         else:
             self.asyncStore = LAsyncStore
 
-        if (metadata): 
+        if metadata:
             self.metadata = metadata
 
     def info(self, msg: object):
-        toLog = f'{self.getMetadataLoggerString()}{msg}'
+        toLog = f"{self.getMetadataLoggerString()}{json.dumps(msg)}"
         self.logger.info(toLog)
 
     def warn(self, msg: object):
-        toLog = f'{self.getMetadataLoggerString()}{msg}'
+        toLog = f"{self.getMetadataLoggerString()}{json.dumps(msg)}"
         self.logger.warn(toLog)
 
     def error(self, msg: object):
-        toLog = f'{self.getMetadataLoggerString()}{msg}'
+        toLog = f"{self.getMetadataLoggerString()}{json.dumps(msg)}"
         self.logger.error(toLog)
 
     def debug(self, msg: object):
-        toLog = f'{self.getMetadataLoggerString()}{msg}'
+        toLog = f"{self.getMetadataLoggerString()}{json.dumps(msg)}"
         self.logger.debug(toLog)
 
     """
     Set metadata to be logged
     """
+
     def setMetadata(self, metadata: dict):
         self.metadata = metadata
 
     """
     Get a pretty printed version of the metadata stored to attach to a log
     """
+
     def getMetadataLoggerString(self):
         metadata = self.metadata
         if metadata and len(metadata) > 0:
@@ -84,4 +97,3 @@ class LLogger:
 
     def runInAsyncContext(self, func):
         asyncio.run(func())
-
